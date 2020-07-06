6ee8ed70a6f567211cacd494fadc5a4c83017e4e
Alex Mezhenskyi <oleksandr.mezhenskyi@agr-software.com>
2020-07-06 22:13:25 +0300

    config:

--- front/api/index.js
+++ front/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: process.env.API_URL || 'http://localhost:8080/api',
+    baseURL: process.env.API_URL || 'http://localhost:3000/api',
     withCredentials: true
 })
 
