76e53abd4b112dbbc4035d5ad1bfc38dbeb8da20
Alex Mezhenskyi <oleksandr.mezhenskyi@agr-software.com>
2020-07-07 09:54:52 +0300

    build: add socket url var

--- front/api/ws.js
+++ front/api/ws.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3000');
+const socket = io(process.env.SOCKET_URL || 'http://localhost:3000');
 
 socket.on('_error', (v) => {
     console.error(v)
