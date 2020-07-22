b934e84599e2b9b7d923c43c65ee492e61d9b27a
Alex Mezhenskyi <oleksandr.mezhenskyi@agr-software.com>
2020-07-22 22:23:57 +0300

    build: favicon blink

---
+++ front/pages/GamePage/PlayerCanvas/DynamicMetaData.js
@@ -0,0 +1,48 @@
+import React, { useRef, useEffect } from 'react'
+import gsap from 'gsap'
+
+export default function DynamicMetaData() {
+    const canvasRef = useRef(null)
+    const favicon = document.getElementById('favicon')
+
+    const loadImage = (src) => {
+        return new Promise(resolve => {
+            const img = new Image();
+            img.onload = () => resolve(img)
+            img.src = src
+        })
+    }
+
+    const createFrames = () => {
+        return Promise.all(['/img/lamp.svg', '/img/lamp-2.svg'].map(loadImage))
+    }
+
+    const updateFavicon = (img, canvasEl, ctx) => {
+        ctx.drawImage(img, 0, 0, canvasEl.width * img.naturalWidth / img.naturalHeight, canvasEl.height)
+        favicon.href = canvasEl.toDataURL('image/png')
+    }
+
+    useEffect(() => {
+        const frames = { number: 0 }
+        const canvasEl = canvasRef.current
+        const ctx = canvasEl.getContext('2d')
+        const tl = gsap.timeline({ repeat: -1, repeatDelay: 0.2, yoyo: true, defaults: { ease: "steps(1)" } })
+
+        createFrames().then((imgs) => {
+            tl.to(frames, {
+                number: imgs.length - 1,
+                onUpdate() {
+                    updateFavicon(imgs[frames.number], canvasEl, ctx)
+                }
+            })
+        })
+
+        return () => {
+            tl.kill()
+            loadImage('/img/lamp-3.svg').then((img) => updateFavicon(img, canvasEl, ctx))
+        }
+    }, [])
+
+    return <canvas ref={canvasRef} width="16px" height="16px"
+                   style={{ visibility: 'hidden', position: 'absolute', width: '16px', height: '16px' }}/>
+}
