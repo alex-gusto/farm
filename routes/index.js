26af705f291d2f43268bc264a66470b20a822209
Alex Mezhenskyi <oleksandr.mezhenskyi@agr-software.com>
2020-07-23 10:51:49 +0300

    test:

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,12 @@ const routes = {
     '/quiz': require('./quiz.routes')
 }
 
+const test = new Router()
+test.get('/*', (ctx) => ctx.body = 'Hello')
 
 Object.entries(routes).forEach(([prefix, route]) => router.use(prefix, route.routes(), route.allowedMethods()))
 
 module.exports = (app) => {
     app.use(router.routes()).use(router.allowedMethods())
+    app.use(test.routes())
 }
