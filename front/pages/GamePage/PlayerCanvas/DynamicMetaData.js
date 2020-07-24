a9fd2cd079f2992325c1b0641ef047d4faacebfb
aleksandr_mezhenskyi <aleksandr.mezhenskyi@gmail.com>
2020-07-24 22:51:35 +0300

    build: add audio for turn

--- front/pages/GamePage/PlayerCanvas/DynamicMetaData.js
+++ front/pages/GamePage/PlayerCanvas/DynamicMetaData.js
@@ -1,48 +1,54 @@
 import React, { useRef, useEffect } from 'react'
 import gsap from 'gsap'
+import { Howl } from 'howler'
 
-export default function DynamicMetaData() {
-    const canvasRef = useRef(null)
-    const favicon = document.getElementById('favicon')
+export default function DynamicMetaData () {
+  const canvasRef = useRef(null)
+  const favicon = document.getElementById('favicon')
 
-    const loadImage = (src) => {
-        return new Promise(resolve => {
-            const img = new Image();
-            img.onload = () => resolve(img)
-            img.src = src
-        })
-    }
+  const loadImage = (src) => {
+    return new Promise(resolve => {
+      const img = new Image()
+      img.onload = () => resolve(img)
+      img.src = src
+    })
+  }
 
-    const createFrames = () => {
-        return Promise.all(['/img/lamp.svg', '/img/lamp-2.svg'].map(loadImage))
-    }
+  const createFrames = () => {
+    return Promise.all([ '/img/lamp.svg', '/img/lamp-2.svg' ].map(loadImage))
+  }
 
-    const updateFavicon = (img, canvasEl, ctx) => {
-        ctx.drawImage(img, 0, 0, canvasEl.width * img.naturalWidth / img.naturalHeight, canvasEl.height)
-        favicon.href = canvasEl.toDataURL('image/png')
-    }
+  const updateFavicon = (img, canvasEl, ctx) => {
+    ctx.drawImage(img, 0, 0, canvasEl.width * img.naturalWidth / img.naturalHeight, canvasEl.height)
+    favicon.href = canvasEl.toDataURL('image/png')
+  }
 
-    useEffect(() => {
-        const frames = { number: 0 }
-        const canvasEl = canvasRef.current
-        const ctx = canvasEl.getContext('2d')
-        const tl = gsap.timeline({ repeat: -1, repeatDelay: 0.2, yoyo: true, defaults: { ease: "steps(1)" } })
+  useEffect(() => {
+    const frames = { number: 0 }
+    const canvasEl = canvasRef.current
+    const ctx = canvasEl.getContext('2d')
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: 0.2, yoyo: true, defaults: { ease: 'steps(1)' } })
+    const howl = new Howl({
+      src: [ '/audio/your-turn.mp3' ],
+      autoplay: true
+    })
 
-        createFrames().then((imgs) => {
-            tl.to(frames, {
-                number: imgs.length - 1,
-                onUpdate() {
-                    updateFavicon(imgs[frames.number], canvasEl, ctx)
-                }
-            })
-        })
-
-        return () => {
-            tl.kill()
-            loadImage('/img/lamp-3.svg').then((img) => updateFavicon(img, canvasEl, ctx))
+    createFrames().then((imgs) => {
+      tl.to(frames, {
+        number: imgs.length - 1,
+        onUpdate () {
+          updateFavicon(imgs[ frames.number ], canvasEl, ctx)
         }
-    }, [])
+      })
+    })
 
-    return <canvas ref={canvasRef} width="16px" height="16px"
-                   style={{ visibility: 'hidden', position: 'absolute', width: '16px', height: '16px' }}/>
+    return () => {
+      tl.kill()
+      howl.unload()
+      loadImage('/img/lamp-3.svg').then((img) => updateFavicon(img, canvasEl, ctx))
+    }
+  }, [])
+
+  return <canvas ref={canvasRef} width="16px" height="16px"
+                 style={{ visibility: 'hidden', position: 'absolute', width: '16px', height: '16px' }}/>
 }
