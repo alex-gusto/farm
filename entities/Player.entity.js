10db5aae2bcba9012c57d0f38d63833aae146e15
aleksandr_mezhenskyi <aleksandr.mezhenskyi@gmail.com>
2020-08-27 21:45:08 +0300

    fix:

--- entities/Player.entity.js
+++ entities/Player.entity.js
@@ -48,7 +48,7 @@ class PlayerEntity {
       // TODO: make it dynamic
       if (!FARM.includes(animal.id)) return acc
 
-      acc[ animal.id ] = 1
+      acc[ animal.id ] = 0
       return acc
     }, {})
   }
